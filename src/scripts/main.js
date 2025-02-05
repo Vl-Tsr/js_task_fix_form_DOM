b03c877f330a2b87cb2012361d81f1757ebbd4b1
Vladyslav Tsarenko <vaaaltaaasaaar@gmail.com>
2025-02-05 11:43:59 -0600

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,15 @@
 'use strict';
 
-let inputElement = document.querySelectorAll('form input')
-
+let inputElement = [...document.querySelectorAll('form input')]
 
 inputElement.forEach( elem => {
 
   let labelElement = document.createElement('label')
   labelElement.classList.add('field-label')
-  labelElement.htmlFor = elem.id
+  labelElement.htmlFor = elem.id || ''
   labelElement.textContent = elem.name.replace(/(?<!\s)([A-Z])/g, ' $1').toUpperCase() || ''
   elem.placeholder = elem.name.replace(/(?<!\s)([A-Z])/g, ' $1') || ''
-  elem.style = 'text-transform: capitalize;'
+  elem.style.textTransform = 'capitalize'
   elem.before( labelElement)
 })
 
